8d900fa6c41e9738f84bdd6b3aba1284912c3367
Mateusz <m.jagienski@gmail.com>
2023-09-24 19:49:54 +0200

    Add record for storing raw ais messages.

---
+++ src/main/java/com/example/aismarinetracker/decoder/RawAisMessage.java
@@ -0,0 +1,5 @@
+package com.example.aismarinetracker.decoder;
+
+public record RawAisMessage(String identifier, int countOfFragments, int fragmentNumber,
+                            String sequentialMessageId, String channelCode, String dataPayload,
+                            int fillBits, String checksum) { }
