a0a1e9cd304234f308a7c97ebf3e96d47fa86fa5
Mateusz <m.jagienski@gmail.com>
2023-10-02 21:55:46 +0200

    Add theme.

--- src/main/java/com/example/aismarinetracker/AisMarineTrackerApplication.java
+++ src/main/java/com/example/aismarinetracker/AisMarineTrackerApplication.java
@@ -1,10 +1,13 @@
 package com.example.aismarinetracker;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Theme("th")
 public class AisMarineTrackerApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
